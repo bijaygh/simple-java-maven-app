efc060e7792459d7a2bc53ac4810595fe46df6fb
bijaygh <110768322+bijaygh@users.noreply.github.com>
2023-06-15 00:10:36 +0545

    Update AppTest.java
    syntax error

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello DevOps!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello DevOps!\");
+            fail("\"message\" is not "\"Hello DevOps!"\");
         }
     }
 
